929fca88dd6514e3526f6cf8ad7119857b2494ff
Dazariath <c17366881@mydit.ie>
2020-03-08 20:22:31 +0000

    added the graph prototype

---
+++ Assets/DarraghFolder/WindowGraph.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WindowGraph : MonoBehaviour
+{
+    [SerializeField] private Sprite circleSprite;
+    private RectTransform graphContainer;
+    public List<int> valueList = new List<int>() {};
+
+    private void Awake()
+    {
+        graphContainer = transform.Find("GraphContainer").GetComponent<RectTransform>();
+
+        ShowGraph(valueList);
+    }
+
+    private void CreateCircle(Vector2 anchoredPosition)
+    {
+        GameObject gameObject = new GameObject("circle", typeof(Image));
+        gameObject.transform.SetParent(graphContainer, false);
+        gameObject.GetComponent<Image>().sprite = circleSprite;
+        RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
+        rectTransform.anchoredPosition = anchoredPosition;
+        rectTransform.sizeDelta = new Vector2(11, 11);
+        rectTransform.anchorMin = new Vector2(0, 0);
+        rectTransform.anchorMax = new Vector2(0, 0);
+    }
+
+    private void ShowGraph(List<int> valueList)
+    {
+        float graphHeight = graphContainer.sizeDelta.y;
+        float yMaximum = 100f;
+        float xSize = 50f;
+        for (int i = 0; i < valueList.Count; i++)
+        {
+            float xPosition = xSize + i * xSize;
+            float yPosition = (valueList[i] / yMaximum) * graphHeight * 5;
+            CreateCircle(new Vector2(xPosition, yPosition));
+        }
+    }
+
+}
