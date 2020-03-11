2df6c3eedacf6b391de253f8bd1341d3b0519658
Dazariath <darraghmnolan@gmail.com>
2020-03-11 17:49:24 +0000

    s

--- Assets/backgrounddecider.cs
+++ Assets/backgrounddecider.cs
@@ -4,10 +4,7 @@ using UnityEngine;
 
 public class backgrounddecider : MonoBehaviour
 {
-  [SerializeField] Sprite[] sprites = new Sprite[1];
-    [SerializeField] string[] mallines = new string[1];
-    [SerializeField] string[] benlines = new string[1];
-    [SerializeField] string[] myslines = new string[1];
+  
 
     // Start is called before the first frame update
     void Start()
